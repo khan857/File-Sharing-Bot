93d5c90f73c6d4caf6a4f3ada5cf8f87c007af28
khan857 <mdaakib01786@gmail.com>
2025-02-23 02:58:36 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b> My Name :</b> <a href='https://t.me/Files_Store9_Bot'>File Store Bot</a> \n<b>Language :</b> <a href='https://python.org'>Python 3</a> \n<b>Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>Server :</b> <a href='https://koyeb.com'>koyeb</a> \n<b>Channel :</b> <a https://t.me/Hindi_Dub_Animes_Official'>Hindi Animes</a> \n<b>Developer :</b> <a href='tg://user?id={OWNER_ID}'>Night</a>",
+            text = f"<b> My Name :</b> <a href='https://t.me/backup916'>File Store Bot</a> \n<b>Language :</b> <a href='https://python.org'>Python 3</a> \n<b>Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>Server :</b> <a href='https://koyeb.com'>koyeb</a> \n<b>Channel :</b> <a https://t.me/backup916'>Hindi Animes</a> \n<b>Developer :</b> <a href='tg://user?id={OWNER_ID}'>Night</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
